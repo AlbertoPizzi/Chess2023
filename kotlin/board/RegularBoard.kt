d582ea5c1a76358e7c4b13b845c6e6da81a82c83
Apetuch <alberto.pizzi@ing.austral.edu.ar>
2023-10-12 14:12:59 -0300

    need help with my board design

--- kotlin/board/RegularBoard.kt
+++ kotlin/board/RegularBoard.kt
@@ -5,6 +5,8 @@ import piece.Piece
 import piece.PieceType
 
 data class RegularBoard(val board: Map<Position, Piece> ) : Board{
+    val colSize = 8
+    val rowSize = 8
     override fun getByColor(color: Color) = board.values.filter { piece -> piece.color == color }
     override fun movePiece(init: Position, final: Position): Board {
         val piece = board[init] ?: throw Exception("No piece at $init")
